7f3a185f1af5934d9a8cfe6dcd25dac4c11f2c43
ariel041596 <chocokises17@gmail.com>
2019-11-19 15:58:42 +0800

    First Commit

--- src/main.js
+++ src/main.js
@@ -4,6 +4,16 @@ import router from "./router";
 
 Vue.config.productionTip = false;
 
+Vue.component("button-counter", {
+  data: function() {
+    return {
+      count: 0
+    };
+  },
+  template:
+    '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
+});
+
 new Vue({
   router,
   render: h => h(App)
